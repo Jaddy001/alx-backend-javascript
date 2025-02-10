94cafec14a77ce4e8b489ee455bd7441e6e8acd7
jaddy001 <jaddyjadnganga@gmail.com>
2025-02-10 18:27:56 +0000

    change2

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,18 +3,23 @@ const http = require('http');
 const port = 1245;
 const host = 'localhost';
 
-// Create server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/test') {
+    res.statusCode = 200;
+    res.end('This is a test page');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
-// Start server
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
 
-// Export server (optional, useful for testing)
 module.exports = server;
-
