469770112362e17ad1c6c49534fead29086cf8a4
jaddy001 <jaddyjadnganga@gmail.com>
2025-02-11 13:13:21 +0000

    changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,8 @@
-process.stdout.write("Welcome to ALX, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
-
-process.on("exit", () => {
-  console.log("This important software is now closing");
-});
-
