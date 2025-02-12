a81f57f94eb45dae385920c9b4ddf7b875256dc9
jaddy001 <jaddyjadnganga@gmail.com>
2025-02-12 07:36:50 +0000

    changes

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,22 +1,59 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
+const PORT = 1245;
 
+// Function to count students
+const countStudents = (databasePath) => new Promise((resolve, reject) => {
+  fs.readFile(databasePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n').filter((line) => line.length > 0);
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    const fields = {};
+    students.forEach(([name, , , field]) => {
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(name);
+    });
+
+    let output = `Number of students: ${students.length}`;
+    Object.entries(fields).forEach(([field, names]) => {
+      output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+    });
+
+    resolve(output);
+  });
+});
+
+// Route: Homepage
 app.get('/', (req, res) => {
   res.send('Hello ALX!');
 });
 
+// Route: Students list
 app.get('/students', (req, res) => {
-  res.write('This is the list of our students\n');
-  countStudents(process.argv[2])
-    .then(() => res.end())
-    .catch((err) => {
-      res.end(`${err.message}\n`);
+  const databasePath = process.argv[2];
+
+  countStudents(databasePath)
+    .then((message) => {
+      res.send(`This is the list of our students\n${message}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
     });
 });
 
-app.listen(1245);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
 module.exports = app;
 
